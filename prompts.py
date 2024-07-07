a4fbab3f565260997db912199ef15d2b324eff44
onlykyps <claudiu.pavel@hotmail.co.uk>
2024-07-07 11:45:26 +0300

    import ollama

--- prompts.py
+++ prompts.py
@@ -2,5 +2,8 @@ import os
 from crewai import Agent, Task, Crew, Process
 from crewai_tools import SerperDevTool
 from langchain_openai import ChatOpenAI
+import ollama
 
 search_tool = SerperDevTool()
+
+# agents definition
