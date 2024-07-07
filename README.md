a22049fc73d696318bc9d567bc4b351c4cd6f36d
onlykyps <claudiu.pavel@hotmail.co.uk>
2024-07-07 15:16:49 +0300

    model agenti AI

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ pip install -q crewai[tools]
 pip install python-dotenv
 https://platform.openai.com/api-keys 
 https://serper.dev/
+
+import ollama
+
+ollama pull mistral
+ollama pull llama2
+ollama pull vicuna
+
--- prompts.py
+++ prompts.py
@@ -7,3 +7,50 @@ import ollama
 search_tool = SerperDevTool()
 
 # agents definition
+
+researcher = Agent(
+    role='Senior Research Assistant',
+    goal='Look up the latest advancements in AI agents',
+    backstory='You work at a leading tech think tank. Your expertise lies in identifying emerging trends. You have a '
+              'knack for dissecting complex data and presenting it as actionable insights',
+    verbose=False,
+    allow_delegation=False,
+    tools=[search_tool],
+    llm=ChatOpenAI(model_name='gpt-3.5-turbo-instruct', temperature=0.7)  # gpt-4-turbo-preview
+)
+
+writer = Agent(
+    role='Tech Content Strategist',
+    goal='Craft compelling content on tech advancements',
+    backstory='you are renowned Content Strategist, known for your insights. You transform complex concepts into '
+              'compelling narratives',
+    verbose=False,
+    allow_delegation=True,
+    tools=[search_tool],
+    llm=ChatOpenAI(model_name='gpt-3.5-turbo-instruct', temperature=0.7)
+)
+
+task1 = Task(
+    description='Conduct comprehensive analysis of the latest advanced tech in AI. Identify key trends, breakthrough '
+                'technologies and potential industry disruptors',
+    expected_output='full analysis report in bullet points',
+    agent=researcher
+)
+
+task2 = Task(
+    description='write a short article that highlights the most significant AI Agent advancements',
+    expected_output='full blogpost of at least 3 paragraphs',
+    agent=writer
+)
+
+# initiate crew and agents
+crew = Crew(
+    agents=[researcher, writer],
+    tasks=[task1, task2],
+    verbose=1,
+)
+
+result = crew.kickoff()
+
+print('########')
+print(result)
